f078bf48198238ab0914c3f2dd0be8fc4f073584
Iain Ballard <iain.ballard@7digital.com>
2013-04-04 17:36:37 +0100

    Solution stub

---
+++ src/DispatchSharp.Unit.Tests/Class1.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace DispatchSharp.Unit.Tests
+{
+	[TestFixture]
+    public class Dummy
+    {
+		[Test]
+		public void dummy_test()
+		{
+			Assert.Inconclusive();
+		}
+    }
+}
---
+++ src/DispatchSharp/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DispatchSharp
+{
+	public class Class1
+	{
+	}
+}
