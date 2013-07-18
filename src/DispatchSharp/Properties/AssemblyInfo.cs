bc7914b1151768a0d5cab5959fedbe35d9331e94
Iain Ballard <iain.ballard@7digital.com>
2013-07-18 16:18:43 +0100

    NuGet package

--- src/DispatchSharp/Properties/AssemblyInfo.cs
+++ src/DispatchSharp/Properties/AssemblyInfo.cs
@@ -1,3 +1,3 @@
 ﻿using System.Reflection;
-[assembly: AssemblyVersion("0.1.8.0")]
-[assembly: AssemblyFileVersion("0.1.8.0")]
+[assembly: AssemblyVersion("0.1.9.0")]
+[assembly: AssemblyFileVersion("0.1.9.0")]
